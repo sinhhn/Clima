816e58bd667efa73546df11f39b5039a7e54ae51
Hoang Ngoc Sinh <h.sinh@bravesoft.co.jp>
2018-10-10 23:40:30 +0900

    Completed

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -11,7 +11,7 @@ import CoreLocation
 import Alamofire
 import SwiftyJSON
 
-class WeatherViewController: UIViewController,CLLocationManagerDelegate {
+class WeatherViewController: UIViewController,CLLocationManagerDelegate, ChangeCityDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
@@ -132,11 +132,25 @@ class WeatherViewController: UIViewController,CLLocationManagerDelegate {
     
     
     //Write the userEnteredANewCityName Delegate method here:
-    
+    func userEnteredANewCityName(city: String) {
+        let params : [String : String] = ["q" : city, "appid" : APP_ID]
+        getWeatherData(url: WEATHER_URL, parametters: params)
+    }
 
     
     //Write the PrepareForSegue Method here
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "changeCityName" {
+            
+            let destinationVC = segue.destination as! ChangeCityViewController
+            
+            
+            destinationVC.delegate = self
+            
+        }
+        
+    }
     
     
     
